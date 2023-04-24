65ceacae9e505cb1821dfbfebecdc21c32ceb618
devzyh <devzyh@foxmail.com>
2023-04-24 18:15:22 +0800

    九格合众钉钉群签到供应商数据提取

--- iunion/ding_supply_to_csv.py
+++ iunion/ding_supply_to_csv.py
@@ -1,14 +1,14 @@
 """
 作者：devzyh
-版本：v1.0.0
+版本：v1.1.0
 描述：转换九格合众钉钉群签到供应商数据为CSV
 """
 import os
 
 # 本次提取变量
-file_path = "file/20230423.txt"  # 文件名称
-check_date = "20230423"  # 签到日期
-salesperson = "欧阳俊杰"  # 销售人员
+check_date = "20230424"  # 签到日期
+salesperson = "销售人员"  # 销售人员
+file_path = "file/" + check_date + ".txt"
 is_kp = "是"  # 是否KP
 
 # 文件存在验证
@@ -20,8 +20,10 @@ if not os.path.exists(file_path):
 with open(file_path, "r", encoding="utf-8") as rf:
     txt = rf.read()
 
-# 键值对齐
-txt = txt + "\n00:00今日结束"
+# 去除无效数据及键值同行
+if txt.find("今日\n签到") > -1:
+    txt = txt.split("今日\n签到")[1]
+txt = txt + "\n23:59今日数据结束"
 txt = txt.replace("签到地点\n", "签到地点@")
 txt = txt.replace("备注\n", "备注@")
 txt = txt.replace("签到图片\n", "签到图片@\n")
@@ -36,71 +38,88 @@ txt = txt.replace("工作区域\n", "工作区域@")
 
 # 循环处理每一行数据
 rows = []
-initMap = {
+default_data = {
+    "time": "",
     "company": "",
     "address": "",
     "phone": "",
     "wechat": "",
     "device": "",
     "qty": "",
-    "operator": ""
+    "operator": "",
+    "area": ""
 }
-map = initMap.copy()
+data = default_data.copy()
 for line in txt.splitlines():
+
+    # 获取本次签到时间，并记录上一条签到数据
+    if len(line) > 2 and ":" == line[2]:
+        # 签到时间
+        data["time"] = line
+
+        # 上一条签到数据
+        if data["phone"] != "":
+            # CSV格式
+            row = check_date + "," + salesperson + "," + data["company"] + "," + data["address"] + "," + is_kp + "," + \
+                  data["phone"] + "," + data["wechat"] + "," + data["device"] + "," + data["qty"] + "," + data[
+                      "operator"]
+            rows.append(row)
+            data = default_data.copy()
+
+        continue
+
+    # 提取键值
     words = line.split("@")
     key = words[0].strip()
+    val = ""
     if len(words) > 1:
         val = words[1].strip()
 
     # 公司名称
     if "公司名称" == key:
-        map["company"] = val
+        data["company"] = val
         continue
 
     # 公司地址
     if "公司位置" == key:
-        map["address"] = val
+        data["address"] = val
         continue
 
     # 联系方式
     if "备注" == key:
-        map["phone"] = val
+        data["phone"] = val
         continue
 
     # 是否添加微信
     if "是否添加微信" == key:
-        map["wechat"] = val
+        data["wechat"] = val
         continue
 
     # 主营设备
     if "设备类型" == key:
         val = val.replace("\"", "").replace("[", "").replace("]", "").replace(",", "、")
-        map["device"] = val
+        data["device"] = val
         continue
 
     # 吨位、高度、型号
     if "吨位、高度、型号" == key:
-        map["device"] = map["device"] + " " + val
+        data["device"] = data["device"] + " " + val
         continue
 
     # 设备数量
     if "设备数量" == key:
-        map["qty"] = val
+        data["qty"] = val
         continue
 
     # 是否有机手
     if "是否有机手" == key:
-        map["operator"] = val
+        data["operator"] = val
         continue
 
-    # 一条次签到结束
-    if ":" == key[2]:
-        # CSV格式
-        row = check_date + "," + salesperson + "," + map["company"] + "," + map["address"] + "," + is_kp + "," + \
-              map["phone"] + "," + map["wechat"] + "," + map["device"] + "," + map["qty"] + "," + map["operator"]
-
-        rows.append(row)
-        map = initMap.copy()
+    # 工作区域
+    if "工作区域" == key:
+        data["area"] = val
+        continue
 
 # 输出到CSV文件
 # 反向输出
